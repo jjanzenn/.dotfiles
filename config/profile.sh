8b5a3d189c313fb1b0149873635533a86b022c59
jjanzen <me@jjanzen.ca>
2025-05-07 13:26:11 -0500

    system changes

--- config/profile.sh
+++
@@ -1,9 +0,0 @@
-export PATH="$HOME/node_modules/.bin:$HOME/bin:$HOME/gems/bin:$HOME/.local/bin:$HOME/go/bin:/usr/local/bin:/Library/TeX/texbin/:/usr/local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-
-export MANPATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/share/man:/usr/share/man:$MANPATH"
-
-if [ "$(uname)" = 'Darwin' ]; then
-    export GPG_TTY=$(tty)
-    export PIPX_HOME="$HOME/.local/pipx"
-    export EDITOR="vim"
-fi
