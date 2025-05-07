56fc2a9d506107dcc59d8c023e39fdc0fd541e28
jjanzen <me@jjanzen.ca>
2025-05-07 11:51:28 -0500

    implement an ansible configuration

--- config/profile.sh
+++ config/profile.sh
@@ -1,4 +1,6 @@
-export PATH="$HOME/node_modules/.bin:$HOME/bin:$HOME/gems/bin:$HOME/.local/bin:$HOME/go/bin:/usr/local/bin:/Library/TeX/texbin/:/opt/homebrew/bin:$PATH"
+export PATH="$HOME/node_modules/.bin:$HOME/bin:$HOME/gems/bin:$HOME/.local/bin:$HOME/go/bin:/usr/local/bin:/Library/TeX/texbin/:/usr/local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+export MANPATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/share/man:/usr/share/man:$MANPATH"
 
 if [ "$(uname)" = 'Darwin' ]; then
     export GPG_TTY=$(tty)
