f06eda53c443f5e287a8d8adc9e4d0bfb122f062
jjanzen <me@jjanzen.ca>
2025-07-08 10:59:01 -0500

    system changes

--- extract_src.py
+++ extract_src.py
@@ -5,29 +5,74 @@ from stat import ST_MODE
 import sys
 import re
 
-with open(sys.argv[1], "r") as inp:
-    with open(sys.argv[2], "w") as out:
-        in_src = False
+def extract(lines, out, startp, endp, quoted, unquoted):
+    in_src = False
+    curr = []
+    min_spaces = -1
+
+    for line in lines:
+        if not in_src and startp.match(line):
+            in_src = True
+        elif in_src and endp.match(line):
+            in_src = False
+            for l in curr:
+                out.write(l[min_spaces:])
+            curr = []
+            min_spaces = -1
+        elif in_src:
+            spaces = len(line) - len(line.lstrip())
+            if min_spaces == -1 or min_spaces > spaces:
+                min_spaces = spaces
+            if quoted != None and unquoted != None:
+                curr.append(re.sub(quoted, unquoted, line))
+
+def main():
+    startp = None
+    endp = None
+    quoted = None
+    unquoted = None
+    if sys.argv[1].endswith(".org"):
         startp = re.compile(r"^\s*#\+begin_src")
         endp = re.compile(r"^\s*#\+end_src")
-        quoted = re.compile(r"^\s*,(\*|,\*|#\+)")
+        quoted = r"^(\s*),(\*|,\*|#\+|,#\+)"
+        unquoted = r"\1\2"
+    elif sys.argv[1].endswith(".md"):
+        startp = re.compile(r"^```")
+        endp = re.compile(r"^```")
 
-        lines = inp.readlines()
-        curr = []
-        min_spaces = -1
-        for line in lines:
-            if startp.match(line):
-                in_src = True
-            elif endp.match(line):
-                in_src = False
-                for l in curr:
-                    out.write(l[min_spaces:])
-                curr = []
-                min_spaces = -1
-            elif in_src:
-                spaces = len(line) - len(line.lstrip())
-                if min_spaces == -1 or min_spaces > spaces:
-                    min_spaces = spaces
-                curr.append(re.sub(r"^(\s*),(\*|,\*|#\+|,#\+)", r"\1\2", line))
+    with open(sys.argv[1], "r") as inp:
+        with open(sys.argv[2], "w") as out:
+            lines = inp.readlines()
 
-        os.chmod(sys.argv[2], os.stat(sys.argv[1])[ST_MODE])
+            extract(lines, out, startp, endp, quoted, unquoted)
+
+            os.chmod(sys.argv[2], os.stat(sys.argv[1])[ST_MODE])
+
+if __name__ == "__main__":
+    main()
+
+# with open(sys.argv[1], "r") as inp:
+#     with open(sys.argv[2], "w") as out:
+#         in_src = False
+#         startp = re.compile(r"^\s*#\+begin_src")
+#         endp = re.compile(r"^\s*#\+end_src")
+# 
+#         lines = inp.readlines()
+#         curr = []
+#         min_spaces = -1
+#         for line in lines:
+#             if startp.match(line):
+#                 in_src = True
+#             elif endp.match(line):
+#                 in_src = False
+#                 for l in curr:
+#                     out.write(l[min_spaces:])
+#                 curr = []
+#                 min_spaces = -1
+#             elif in_src:
+#                 spaces = len(line) - len(line.lstrip())
+#                 if min_spaces == -1 or min_spaces > spaces:
+#                     min_spaces = spaces
+#                 curr.append(re.sub(r"^(\s*),(\*|,\*|#\+|,#\+)", r"\1\2", line))
+# 
+#         os.chmod(sys.argv[2], os.stat(sys.argv[1])[ST_MODE])
